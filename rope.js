db44a0cbfb9108d7a53e70d8b0fe6a83dde76126
NEERAJCODER20008 <77783301+NEERAJCODER20008@users.noreply.github.com>
2021-06-19 20:29:20 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,27 @@
+class Rope{
+    constructor(body1,point1){
+     
+        var options={
+            bodyA:body1,
+            pointB:point1,
+          
+        }
+        this.rope=Constraint.create(options)
+        this.pointB=point1
+        World.add(world,this.rope)
+    }
+    fly(){
+        this.rope.bodyA=null
+    }
+    display(){
+        if(this.rope.bodyA){
+        var pointA=this.rope.bodyA.position;
+        var pointB=this.pointB;
+
+        strokeWeight(2);
+
+      
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
+}
